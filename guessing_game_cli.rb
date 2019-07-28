fda89f4abe658ea78f4de38894ad3cbf1abc5d5f
Haim D <haim11293@aol.com>
2019-07-28 16:59:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   random_number = rand(1..6).to_s
   user_input = gets.chomp
-  
+
   if user_input == random_number
     puts "You guessed the correct number!"
   elsif user_input != random_number
@@ -14,4 +14,3 @@ def run_guessing_game
     puts "Invalid input, please guess a number between 1 and 6"
   end
 end
-
